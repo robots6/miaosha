98939192a32aed1259e2f47ccf7418288fcbaf64
lucq <lcq178433837>
2020-10-20 22:39:20 +0800

    修改

--- src/main/java/com/miaoshaproject/controller/OrderController.java
+++ src/main/java/com/miaoshaproject/controller/OrderController.java
@@ -1,14 +1,16 @@
 package com.miaoshaproject.controller;
 
 import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
 import com.miaoshaproject.response.CommonReturntype;
 import com.miaoshaproject.service.OrderService;
 import com.miaoshaproject.service.model.OrderModel;
+import com.miaoshaproject.service.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller("order")
 @RequestMapping("/order")
@@ -18,14 +20,24 @@ public class OrderController extends BaseController{
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
     //封装下单请求
+    @RequestMapping(value = "/createorder", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
     public CommonReturntype creatOrder(@RequestParam(name = "itemId")Integer itemId,
                                        @RequestParam(name = "amount")Integer amount) throws BusinessException {
 
+        Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
+        if (isLogin==null||!isLogin.booleanValue()){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"用户还未登陆，不能下单");
+        }
+
         //获取用户的登陆信息
+        UserModel userModel = (UserModel) httpServletRequest.getSession().getAttribute("LOGIN_USER");
 
-
-        OrderModel orderModel = orderService.creatOrder(null,itemId,amount);
+        OrderModel orderModel = orderService.creatOrder(userModel.getId(),itemId,amount);
         return CommonReturntype.create(null);
     }
 }
